00548cbbd6e7937932cf9b59e487ecdd27de0860
Sh1ze96 <ruzhov.lekha@gmail.com>
2023-12-15 01:22:32 +0300

    GunB

---
+++ GunsBams/Classes/Connect.cs
@@ -0,0 +1,14 @@
+﻿using GunsBams.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunsBams.Classes
+{
+    class Connect
+    {
+        public static GunBamsEntities3 modeldb;
+    }
+}
